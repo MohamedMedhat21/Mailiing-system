111961592c227fdd7e83cb7670d31a47fded5154
Mohamed Medhat <blackmanmido21@gmail.com>
2019-12-01 14:12:52 +0200

    initialization 2

--- Mailing-system-V2/Mailing-system-V2/Login.xaml.cs
+++ Mailing-system-V2/Mailing-system-V2/Login.xaml.cs
@@ -10,16 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
+
 namespace MailingSystem
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for Loginxaml.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Loginxaml : Window
     {
-        public MainWindow()
+        public Loginxaml()
         {
             InitializeComponent();
         }
